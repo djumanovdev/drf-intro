a887a83fe1f599ae50f2ab437aefd34735e11792
djumanov <djumanovdev@gmail.com>
2025-11-28 18:58:17 +0500

    done

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Item
 
 
-class ItemSerializer(ModelSerializer):
+class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+
+class FilterItemSerialer(serializers.Serializer):
+    status = serializers.BooleanField(required=False)
+    min_amount = serializers.FloatField(required=False)
+    max_amount = serializers.FloatField(required=False)
--- api/views.py
+++ api/views.py
@@ -4,9 +4,29 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet 
 
 from .models import Item
-from .serializers import ItemSerializer
+from .serializers import ItemSerializer, FilterItemSerialer
 
 
 class ItemsViewSet(ModelViewSet):
-    queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        queryset = Item.objects.all()
+
+        if self.action == 'list':
+            filter_serializer = FilterItemSerialer(data=self.request.query_params)
+
+            if filter_serializer.is_valid(raise_exception=True):
+                status = filter_serializer.data.get('status')
+                min_amount = filter_serializer.data.get('min_amount')
+                max_amount = filter_serializer.data.get('max_amount')
+
+                if status is not None:
+                    queryset = queryset.filter(status=status)
+                if min_amount is not None:
+                    queryset = queryset.filter(amount__gte=min_amount)
+                if max_amount is not None:
+                    queryset = queryset.filter(amount__lte=max_amount)
+        
+        return queryset
+    
